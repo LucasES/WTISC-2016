efaab1408784846570bb1182cdf438fc13fd7d3f
LucasES <lucas.compufc@gmail.com>
2016-05-15 00:35:35 -0300

    Fix repositório de categorias.

--- src/main/java/br/com/ufc/repository/CategoryRepository.java
+++ src/main/java/br/com/ufc/repository/CategoryRepository.java
@@ -3,7 +3,9 @@ package br.com.ufc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.ufc.model.Category;
+
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryRepository, Long>{
+public interface CategoryRepository extends JpaRepository<Category, Long>{
 
 }
