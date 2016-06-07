7d3726b5776999de699d2160fc0ee96cc8db7cef
LucasES <lucas.compufc@gmail.com>
2016-06-06 21:04:41 -0300

    Adicionando extensão da interface Specification.

--- src/main/java/br/com/ufc/repository/BuyRepository.java
+++ src/main/java/br/com/ufc/repository/BuyRepository.java
@@ -1,11 +1,12 @@
 package br.com.ufc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import br.com.ufc.model.Buy;
 
 @Repository
-public interface BuyRepository extends JpaRepository<Buy, Long> {
+public interface BuyRepository extends JpaRepository<Buy, Long>, JpaSpecificationExecutor<Buy>{
 
 }
